ff1930631a0d4938dbc724bb452c41eb4f1c3a7c
bonaime <bonaime@ipgp.fr>
2013-04-17 13:16:26 +0200

    default scale was 1 ! Now autoscale if no scale parameter

--- seedlink_plotter.py
+++ seedlink_plotter.py
@@ -232,7 +232,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--channel', type=str,
                         help='the name of the channel to plot', required=True)
     parser.add_argument(
-        '--scale', type=int, help='the scale to apply on data ex:50000', required=False, default=1)
+        '--scale', type=int, help='the scale to apply on data ex:50000', required=False)
 
     # Real-time parameters
     parser.add_argument('--seedlink_server', type=str,
