68b104dab38f276039566295445bfe0fef05c9e8
bonaime <bonaime@ipgp.fr>
2015-07-08 14:56:18 +0200

    compare x_scale and backtrace_time in seconds

--- seedlink_plotter/seedlink_plotter.py
+++ seedlink_plotter/seedlink_plotter.py
@@ -518,10 +518,15 @@ def main():
     args = parser.parse_args()
 
  # Check x_scale and backtrace_time in drum plot mode
-    if not args.line_plot and (args.backtrace_time % args.x_scale):
+    if not args.line_plot and (args.backtrace_time % (args.x_scale*60)):
         print"Backtrace_time should be a multiple of x_scale (default 60m)"
         print "Aborting."
         sys.exit()
+    if not args.line_plot and (args.backtrace_time % (args.x_scale*60)):
+        print"Backtrace_time should be a multiple of x_scale (default 60m)"
+        print "Aborting."
+        sys.exit()
+
 
     if args.verbose:
         loglevel = logging.DEBUG
